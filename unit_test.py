429cd12fe9feb1d05712a85090e28ede93e20f9e
Umair Khushnood <umairk@Umairs-MacBook-Pro.local>
2023-04-04 14:08:55 -0400

    buildspec file plus some changes to unit test

--- unit_test.py
+++ unit_test.py
@@ -2,13 +2,35 @@ from google.cloud import bigquery
 from src.bigquery_crud_ops import BQdataset
 import os
 import argparse
-
+from google.oauth2 import service_account
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--google_cred_path", type=str, required=True)
+parser.add_argument("--google_key", type=str, required=True)
 args = parser.parse_args()
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.google_cred_path
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.google_key
+
+# Downloaded credentials in JSON format
+
+gcp_sa_credentials = {
+    "type": "service_account",
+    "project_id": "ukhushn-proj2",
+    "private_key_id": "6e68152d46b68cf112d4eaef094a322da32443fe",
+    "private_key": "",
+    "client_email": "ukhushn-try@ukhushn-proj2.iam.gserviceaccount.com",
+    "client_id": "106069951904078738006",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ukhushn-try%40ukhushn-proj2.iam.gserviceaccount.com"
+}
+
+gcp_sa_credentials["private_key"] = args.google_key
+
+project_id = gcp_sa_credentials["project_id"]
+
+credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
+client = bigquery.Client(project=project_id, credentials=credentials)
 
 
 def unit_test_class_dataset(dataset_name):
@@ -23,6 +45,6 @@ def unit_test_class_dataset(dataset_name):
 
 
 # FIRE UP THE CLIENT
-client = bigquery.Client()
+# client = bigquery.Client()
 # Test the func
 unit_test_class_dataset('test1')
