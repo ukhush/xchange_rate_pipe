e6d442e6846a30912ea8df62884c2c28af1cec0d
Umair Khushnood <umairk@Umairs-MacBook-Pro.local>
2023-04-08 13:20:38 -0400

    commit unit test updates to unit test

--- unit_test.py
+++ unit_test.py
@@ -5,33 +5,34 @@ from google.oauth2 import service_account
 import json
 import unittest
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--google_key", required=True)
-args = parser.parse_args()
 
-# Downloaded credentials in JSON format
-
-gcp_sa_credentials = {
-    "type": "service_account",
-    "project_id": "ukhushn-proj2",
-    "private_key_id": "6e68152d46b68cf112d4eaef094a322da32443fe",
-    "private_key": "",
-    "client_email": "ukhushn-try@ukhushn-proj2.iam.gserviceaccount.com",
-    "client_id": "106069951904078738006",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ukhushn-try%40ukhushn-proj2.iam.gserviceaccount.com"
-}
-
-gcp_sa_credentials["private_key"] = json.loads(args.google_key)
-
-project_id = gcp_sa_credentials["project_id"]
-credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
-client = bigquery.Client(project=project_id, credentials=credentials)
 
 
 class TESTSforDATASETclass(unittest.TestCase):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--google_key", required=True)
+    args = parser.parse_args()
+    # Downloaded credentials in JSON format
+
+    gcp_sa_credentials = {
+        "type": "service_account",
+        "project_id": "ukhushn-proj2",
+        "private_key_id": "6e68152d46b68cf112d4eaef094a322da32443fe",
+        "private_key": "",
+        "client_email": "ukhushn-try@ukhushn-proj2.iam.gserviceaccount.com",
+        "client_id": "106069951904078738006",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ukhushn-try%40ukhushn-proj2.iam.gserviceaccount.com"
+    }
+
+    gcp_sa_credentials["private_key"] = json.loads(args.google_key)
+
+    project_id = gcp_sa_credentials["project_id"]
+    credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
+    client = bigquery.Client(project=project_id, credentials=credentials)
 
     def TEST1(self):
         bqdataset = BQdataset(client)
