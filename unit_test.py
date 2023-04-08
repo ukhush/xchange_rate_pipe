02ba912593ce2df574628c9c252da46caa3a1cfb
Umair Khushnood <umairk@Umairs-MacBook-Pro.local>
2023-04-08 13:30:01 -0400

    commit unit test updates to unit test number 2

--- unit_test.py
+++ unit_test.py
@@ -7,9 +7,22 @@ import unittest
 
 
 
-
 class TESTSforDATASETclass(unittest.TestCase):
 
+    def TEST1(self):
+        bqdataset = BQdataset(client)
+        project = bqdataset.project()
+        outmsg = bqdataset.create_dataset('TEST_dataset', 'US')
+        test_msg = f"Created dataset {project}.TEST_dataset"
+        self.assertEqual(outmsg, test_msg, msg=outmsg)
+
+    def TEST2(self):
+        bqdataset = BQdataset(client)
+        outmsg = bqdataset.delete_dataset('TEST_dataset')
+        test_msg = "Deleted dataset TEST_dataset"
+        self.assertEqual(outmsg, test_msg, msg=outmsg)
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--google_key", required=True)
     args = parser.parse_args()
@@ -34,18 +47,4 @@ class TESTSforDATASETclass(unittest.TestCase):
     credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
     client = bigquery.Client(project=project_id, credentials=credentials)
 
-    def TEST1(self):
-        bqdataset = BQdataset(client)
-        project = bqdataset.project()
-        outmsg = bqdataset.create_dataset('TEST_dataset', 'US')
-        test_msg = f"Created dataset {project}.TEST_dataset"
-        self.assertEqual(outmsg, test_msg, msg=outmsg)
-
-    def TEST2(self):
-        bqdataset = BQdataset(client)
-        outmsg = bqdataset.delete_dataset('TEST_dataset')
-        test_msg = "Deleted dataset TEST_dataset"
-        self.assertEqual(outmsg, test_msg, msg=outmsg)
-
-if __name__ == '__main__':
     unittest.main()
